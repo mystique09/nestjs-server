db2f7c88f7f6481731611d56b994f3d3f84bf862
unknown <knightkrypto7@gmail.com>
2021-12-14 16:37:01 +0800

    add validator and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [".env", ".development.env"]
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
