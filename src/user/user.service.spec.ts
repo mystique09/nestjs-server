77569ffd6ec73fa80b843b7ca69dddbb4a1613c5
unknown <knightkrypto7@gmail.com>
2021-12-15 10:57:13 +0800

    add user test case

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -15,4 +15,22 @@ describe('UserService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return an error when creating user', async () => {
+    const response = await service.create({username: 'testuser123', password: '', email: 'testemail@gmail.com'});
+
+    expect(response === "User created!").toBeFalsy();
+  });
+
+  it('should create new user', async () => {
+    const response = await service.create({username: 'testuser123', password: 'testpass', email: 'testemail@gmail.com'});
+
+    expect(response).toEqual("User created!");
+  });
+
+  it('should return all user', async () => {
+    const response = await service.findAll();
+
+    expect(response.length).toBeInstanceOf(Object);
+  });
 });
